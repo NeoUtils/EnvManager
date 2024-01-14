76b2cd73402afcfa32b6e792d37665eb06029d01
Irineu333 <mmmirineusilva@gmail.com>
2024-01-13 21:51:28 -0300

    chore: add support for envm command in project folder on windows

---
+++ envm.bat
@@ -0,0 +1,5 @@
+@rem Use this script to test EnvManager on Windows from the command line.
+@rem Use ".\envm <options> <command> <args>" to test.
+@rem Use ".\gradlew installDist" to generate the bin files.
+
+.\build\install\EnvManager\bin\EnvManager %*
