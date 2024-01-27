f71f9156523c9b5e6afe164b0616eca27c6876b7
Irineu333 <mmmirineusilva@gmail.com>
2024-01-26 23:44:56 -0300

    feat(remove): implement remove from environment

--- src/main/kotlin/com/neo/envmanager/command/Remove.kt
+++ src/main/kotlin/com/neo/envmanager/command/Remove.kt
@@ -3,11 +3,18 @@ package com.neo.envmanager.command
 import com.github.ajalt.clikt.core.Abort
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.google.gson.Gson
 import com.neo.envmanager.core.Command
+import com.neo.envmanager.exception.error.EnvironmentNotFound
+import com.neo.envmanager.exception.error.SpecifyEnvironmentError
 import com.neo.envmanager.exception.error.SpecifyKeysError
 import com.neo.envmanager.exception.error.KeyNotFound
 import com.neo.envmanager.exception.error.TargetNotFound
 import com.neo.envmanager.model.Config
+import com.neo.envmanager.util.extension.json
+import com.neo.envmanager.util.extension.readAsMap
 import com.neo.envmanager.util.extension.readAsProperties
 import com.neo.envmanager.util.extension.requireInstall
 import java.io.File
@@ -20,20 +27,86 @@ class Remove : Command(
         help = "Properties tags to delete, separated by space"
     ).multiple()
 
+    private val tag by option(
+        names = arrayOf("-t", "--tag"),
+        help = "Environment tag"
+    )
+
+    private val targetOnly by option(
+        names = arrayOf("-o", "--target-only"),
+        help = "Delete properties from target only"
+    ).flag()
+
     private lateinit var config: Config
 
     override fun run() {
 
         config = requireInstall()
 
-        removeInTarget()
-    }
-
-    private fun removeInTarget() {
-
         if (keys.isEmpty()) {
             throw SpecifyKeysError()
         }
+
+        if (targetOnly) {
+            removeInTarget()
+            return
+        }
+
+        removeFromEnvironment()
+    }
+
+    private fun removeFromEnvironment() {
+
+        val tag = tag ?: config.currentEnv ?: throw SpecifyEnvironmentError()
+
+        val environment = paths.environmentsDir.resolve(tag.json)
+
+        if (!environment.exists()) {
+            throw EnvironmentNotFound(tag)
+        }
+
+        val properties = environment.readAsMap().toMutableMap()
+
+        val keys = keys.mapNotNull { key ->
+            if (properties.containsKey(key)) {
+                key
+            } else {
+                echoFormattedHelp(KeyNotFound(key))
+                null
+            }
+        }
+
+        if (keys.isEmpty()) throw Abort()
+
+        keys.forEach { properties.remove(it) }
+
+        environment.writeText(
+            Gson().toJson(
+                properties
+            )
+        )
+
+        // Checkout when set in current environment
+        if (tag == config.currentEnv) {
+            checkout(tag)
+        }
+    }
+
+    private fun checkout(tag: String) {
+
+        val target = File(config.targetPath)
+
+        val environment = paths.environmentsDir.resolve(tag.json)
+
+        target.writeText(
+            environment
+                .readAsMap()
+                .entries
+                .joinToString(separator = "\n")
+        )
+    }
+
+    private fun removeInTarget() {
 
         val target = File(config.targetPath)
 
