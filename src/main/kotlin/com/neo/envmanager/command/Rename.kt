dac2cf6889bb89e736cfa0c576049ede88826069
Irineu333 <mmmirineusilva@gmail.com>
2024-01-28 21:04:50 -0300

    refactor(rename): implement renameTo in Environment

--- src/main/kotlin/com/neo/envmanager/command/Rename.kt
+++ src/main/kotlin/com/neo/envmanager/command/Rename.kt
@@ -1,11 +1,8 @@
 package com.neo.envmanager.command
 
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.google.gson.Gson
-import com.neo.envmanager.com.neo.envmanager.exception.error.EnvironmentAlreadyExists
 import com.neo.envmanager.core.Command
 import com.neo.envmanager.model.Environment
-import com.neo.envmanager.util.extension.json
 import com.neo.envmanager.util.extension.requireInstall
 import com.neo.envmanager.util.extension.update
 
@@ -25,13 +22,7 @@ class Rename : Command(
 
         val config = requireInstall()
 
-        val oldEnvironment = Environment.get(paths.environmentsDir, oldTag)
-
-        val newEnvironment = paths.environmentsDir.resolve(newTag.json)
-
-        if (newEnvironment.exists()) throw EnvironmentAlreadyExists(newTag)
-
-        oldEnvironment.file.renameTo(newEnvironment)
+         Environment.get(paths.environmentsDir, oldTag).renameTo(newTag)
 
         if (oldTag == config.currentEnv) {
             config.update {
--- src/main/kotlin/com/neo/envmanager/model/Environment.kt
+++ src/main/kotlin/com/neo/envmanager/model/Environment.kt
@@ -1,6 +1,7 @@
 package com.neo.envmanager.model
 
 import com.google.gson.Gson
+import com.neo.envmanager.com.neo.envmanager.exception.error.EnvironmentAlreadyExists
 import com.neo.envmanager.exception.error.EnvironmentNotFound
 import com.neo.envmanager.util.extension.json
 import com.neo.envmanager.util.extension.readAsMap
@@ -23,6 +24,17 @@ value class Environment(val file: File) {
         file.writeText(
             Gson().toJson(properties)
         )
+    }
+
+    fun renameTo(tag: String) : Environment {
+
+        val newFile = file.parentFile.resolve(tag.json)
+
+        if (newFile.exists()) throw EnvironmentAlreadyExists(tag)
+
+        file.renameTo(newFile)
+
+        return Environment(newFile)
     }
 
     companion object {
