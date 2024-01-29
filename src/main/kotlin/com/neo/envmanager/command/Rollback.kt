a2afa2ec95af9cc9cc2445c829e7e66e33fe291b
Irineu333 <mmmirineusilva@gmail.com>
2024-01-29 10:54:23 -0300

    fix(rollback): rollback always must be from the current environment

--- src/main/kotlin/com/neo/envmanager/command/Rollback.kt
+++ src/main/kotlin/com/neo/envmanager/command/Rollback.kt
@@ -1,8 +1,7 @@
 package com.neo.envmanager.command
 
-import com.github.ajalt.clikt.parameters.options.option
+import com.neo.envmanager.com.neo.envmanager.exception.error.NoCurrentEnvironment
 import com.neo.envmanager.core.Command
-import com.neo.envmanager.exception.error.SpecifyEnvironmentError
 import com.neo.envmanager.model.Environment
 import com.neo.envmanager.model.Target
 import com.neo.envmanager.util.extension.requireInstall
@@ -11,18 +10,13 @@ class Rollback : Command(
     help = "Rollback the target"
 ) {
 
-    private val tag by option(
-        names = arrayOf("-t", "--tag"),
-        help = "Environment tag"
-    )
-
     override fun run() {
 
         val config = requireInstall()
 
         val target = Target.getOrCreate(config.targetPath)
 
-        val tag = tag ?: config.currentEnv ?: throw SpecifyEnvironmentError()
+        val tag = config.currentEnv ?: throw NoCurrentEnvironment()
 
         val environment = Environment.get(paths.environmentsDir, tag)
 
